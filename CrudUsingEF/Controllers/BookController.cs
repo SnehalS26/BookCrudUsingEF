00fb557728ea792760773dbe9e2ba9840110ab07
SnehalS26 <Snehalshirsath26@gmail.com>
2023-09-09 21:47:44 +0530

    Add project files.

---
+++ CrudUsingEF/Controllers/BookController.cs
@@ -0,0 +1,105 @@
+﻿using CrudUsingEF.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CrudUsingEF.Controllers
+{
+    public class BookController : Controller
+    {
+        ApplicationDbContext context;
+        BookCrud bookCrud;
+        public BookController(ApplicationDbContext context)
+        {
+            this.context = context;
+            bookCrud = new BookCrud(this.context);
+        }
+        // GET: BookController
+        public ActionResult Index()
+        {
+            var model = bookCrud.GetBooks();
+            return View(model);
+        }
+
+        // GET: BookController/Details/5
+        public ActionResult Details(int id)
+        {
+            return View(bookCrud.GetBookById(id));
+        }
+
+        // GET: BookController/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: BookController/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(Book book)
+        {
+            try
+            {
+                var res = bookCrud.AddBook(book);
+                if(res == 1)
+                return RedirectToAction(nameof(Index));
+                else
+                    return View();
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: BookController/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View(bookCrud.GetBookById(id));
+        }
+
+        // POST: BookController/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(Book book)
+        {
+            try
+            {
+                var res = bookCrud.UpdateBook(book);
+                if (res == 1)
+                    return RedirectToAction(nameof(Index));
+                else
+                    return View();
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: BookController/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View(bookCrud.GetBookById(id));
+        }
+
+        // POST: BookController/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        [ActionName("Delete")]
+        public ActionResult DeleteConfirm(int id)
+        {
+            try
+            {
+                var res = bookCrud.DeleteBook(id);
+                if (res == 1)
+                    return RedirectToAction(nameof(Index));
+                else
+                    return View();
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ CrudUsingEF/Models/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace CrudUsingEF.Models
+{
+    public class ApplicationDbContext:DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options):base(options)
+        {
+            
+        }
+        //fetch the date from DB and store at application side
+        //Dbset will translate LINQ quries in SQL and fire in DB
+        public DbSet<Book> books { get; set; }
+    }
+}
